6f6a916a6cfe7b706ab56ee0533b072e6aee4838
Mark Bradley <barkmadley@gmail.com>
2013-12-12 23:48:52 +1100

    from monolithic functions to middleware modules
    could improve testability. might look at some of the web
    combinators used in other frameworks for inspiration later

--- src/main.ml
+++ src/main.ml
@@ -1,40 +1,67 @@
 open Core.Std
 open Async.Std
 
-let port_arg () =
-  Command.Spec.(
-    flag "-port" (optional_with_default 8080 int)
-      ~doc:"port"
-  )
+(** [Middleware] lazy body + request -> lazy response *)
+module type Middleware =
+  sig
+    val run : body:string list Deferred.t -> Cohttp_async.Request.t -> Cohttp_async.Server.response Deferred.t
+  end
 
-(* A very basic command-line program, using Command, Core's Command line
-   parsing library.  *)
+(** [EchoMiddleware] pipe the body of the request to the body of the response
+  *)
+module EchoMiddleware : Middleware =
+  struct
+    let run ~body (_request: Cohttp_async.Request.t) =
+    begin
+      body >>= fun lines ->
+      Cohttp_async.Server.respond_with_string (String.concat lines)
+    end
+  end
 
-let command =
+(** [runmiddleware]
+  * convert the string Pipe.Reader.t option to [`Eof|`Ok of string] Deferred.t
+  * strip the socket option as I am currently not using it.
+  *)
+let runmiddleware middleware ~body _socket _request =
+  let pipe_content : string list Deferred.t  =
+    match body with
+    | None      -> return []
+    | Some pipe -> Pipe.to_list pipe
+  in
+  middleware ~body:pipe_content _request
+
+(** [start_server] with a particular middleware on a particular port *)
+let start_server middleware ~port () =
+  let _server : (Socket.Address.Inet.t, int) Cohttp_async.Server.t Deferred.t =
+    Cohttp_async.Server.(
+      create
+        (Tcp.on_port port)
+        (runmiddleware middleware)
+    )
+  in
+  Deferred.never ()
+
+(** A very basic command-line program, using Command, Core's Command line
+  * parsing library.
+  *)
+let start_server_command =
   (** [Command.async_basic] is used for creating a command.
     * Every command takes a text summary and a command line spec
     *  as well as the commands implementation
     *)
   Command.async_basic
-    ~summary:"twipper"
+    ~summary:"start server"
     (** Command line specs are built up component by component, using a small
       * combinator library whose operators are contained in [Command.Spec]
       *)
     Command.Spec.(
-      empty +> port_arg ()
+      (** convert the port argument to a named argument *)
+      step (fun m port -> m ~port)
+      +> flag "-port" (optional_with_default 8080 int) ~doc:"port"
     )
     (** The command-line spec determines the argument to this function, which
       * show up in an order that matches the spec.
       *)
-    (fun port () ->
-      let _ =
-        Cohttp_async.Server.(
-          create
-          (Tcp.on_port port)
-          (fun ~body _ _ -> respond_with_string "hello world")
-        )
-      in
-      Deferred.never ()
-    )
+    (start_server EchoMiddleware.run)
 
-let () = Command.run command
+let () = Command.run start_server_command
