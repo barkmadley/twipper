3f0b0f54a0f9bcb4ac9d3255d8ad72381d2cf818
Mark Bradley <barkmadley@gmail.com>
2013-12-17 21:20:57 +1100

    handle the form that creates a tweep
    This also means that the list of tweeps need to be mutable

--- src/api/api.ml
+++ src/api/api.ml
@@ -13,7 +13,7 @@ let users =
     name = "Someone"
   }
 
-let tweeps =
+let tweeps = ref
   [
     {
       uuid = Tweep_UUID.of_string "12345";
@@ -27,20 +27,26 @@ let tweeps =
     };
   ]
 
+let tweep_uri base_uri tweep =
+  Printf.sprintf "%s/tweep/%s" base_uri (Tweep_UUID.to_string tweep.uuid)
+
+let user_uri base_uri user_uuid =
+  Printf.sprintf "%s/user/%s" base_uri (User_UUID.to_string user_uuid)
+
 let tweep_to_html prefix suffix base_uri tweep =
   Printf.sprintf
 "
 %s
 <div itemtype=\"\">
-  <a rel=self href=\"%s/tweep/%s\">self</a>
-  <a itemprop=user href=\"%s/user/%s\">User</a>
+  <a rel=self href=\"%s\">self</a>
+  <a itemprop=user href=\"%s\">User</a>
   <span itemprop=text>%s</span>
 </div>
 %s
 "
   prefix
-  base_uri (Tweep_UUID.to_string tweep.uuid)
-  base_uri (User_UUID.to_string tweep.user_uuid)
+  (tweep_uri base_uri tweep)
+  (user_uri base_uri tweep.user_uuid)
   tweep.text
   suffix
 
@@ -57,7 +63,6 @@ struct
     match R.meth request, Uri.path (R.uri request) with
     | `GET, "/" ->
     begin
-
       S.respond_with_string (
         Printf.sprintf
 "
@@ -67,16 +72,39 @@ struct
       %s
     </ul>
     <form data-rel=\"tweep\" method=\"POST\" action=\"%s/tweep\">
-      <input type=hidden name=user_uuid value=\"%s\" />
-      <input type=text name=text />
+      <input type=\"hidden\" name=\"user_uuid\" value=\"%s\" />
+      <input type=\"text\" name=\"text\" />
     </form>
   </body>
 </html>
 "
-        (String.concat ~sep:"\n" (List.map ~f:(tweep_to_html "<li>" "</li>" host) tweeps))
+        ( String.concat ~sep:"\n" (
+            List.map ~f:(tweep_to_html "<li>" "</li>" host) !tweeps)
+        )
         host
         (User_UUID.to_string users.uuid)
       )
+    end
+    | `POST, "/tweep" ->
+    begin
+      body >>= fun body ->
+      let post_body = Uri.query_of_encoded body in
+      let text_o = List.Assoc.find post_body "text" in
+      let text_s = Option.value text_o ~default:[] in
+      let text = Option.value (List.hd text_s) ~default:"" in
+      let new_tweep =
+        {
+          uuid = Tweep_UUID.of_string "testing";
+          text = text;
+          user_uuid = users.uuid;
+        }
+      in
+      tweeps := new_tweep :: !tweeps;
+      let headers =
+        Cohttp.Header.init_with "location" (tweep_uri host new_tweep)
+      in
+      let body = Pipe.of_list [body] in
+      return (S.respond ~headers `Created ~body:(Some body))
     end
     | `GET, uri when String.is_prefix uri ~prefix:"/tweep/" ->
     begin
@@ -84,7 +112,7 @@ struct
       | None -> S.respond_with_string "404" (* Should not happen *)
       | Some suffix ->
         let uuid_suffix = Tweep_UUID.of_string suffix in
-        match List.findi ~f:(fun i t -> t.uuid = uuid_suffix) tweeps with
+        match List.findi ~f:(fun i t -> t.uuid = uuid_suffix) !tweeps with
         | None -> S.respond_with_string "404"
         | Some (i, tweep) ->
           S.respond_with_string (
